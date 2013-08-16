62c36f94c5b49120ab53a9093dead3b004673322
TripNRaVeR <noerialexeew@live.nl>
2013-08-16 22:12:17 +0200

    fixup identification

--- cm.mk
+++ cm.mk
@@ -12,9 +12,9 @@ $(call inherit-product, vendor/tripndroid/config/nfc_enhanced.mk)
 $(call inherit-product, vendor/tripndroid/config/common_full_phone.mk)
 
 PRODUCT_NAME :=  tripndroid_m7
-PRODUCT_BRAND := htc
+PRODUCT_BRAND := htc_europe
 PRODUCT_DEVICE := m7
 PRODUCT_MODEL := HTC One
 PRODUCT_MANUFACTURER := HTC
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7_google BUILD_ID=JWR66V.H10 BUILD_FINGERPRINT="htc/m7_google/m7:4.3/JWR66V.H10/230993:user/release-keys" PRIVATE_BUILD_DESC="3.06.1700.10 CL230993 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7_google BUILD_ID=JWR66V BUILD_FINGERPRINT="htc/m7_google/m7:4.3/JWR66V.H10/230993:user/release-keys" PRIVATE_BUILD_DESC="3.06.1700.10 CL230993 release-keys"
