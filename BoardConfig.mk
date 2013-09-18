bbd6fbe6dbddeafb99db59340213a887e638a4bc
TripNRaVeR <noerialexeew@live.nl>
2013-09-18 22:57:35 +0200

    dont need to define nr buffers

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,9 +105,6 @@ TARGET_USES_ION := true
 USE_OPENGL_RENDERER := true
 BOARD_EGL_CFG := device/htc/m7/egl.cfg
 
-# Graphics - FB
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 4
-
 # Graphics - Skia
 BOARD_USE_SKIA_LCDTEXT := true
 BOARD_USES_SKIAHWJPEG := true
